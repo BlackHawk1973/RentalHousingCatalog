ad0571d31f16d13f7d9172d09ba07eb68ef23643
Black Hawk <bhawk73@gmail.com>
2023-10-11 12:41:36 +0300

    Add simple ApplicationController

--- src/main/java/org/top/rentalhousingcatalog/ApplicationController.java
+++ src/main/java/org/top/rentalhousingcatalog/ApplicationController.java
@@ -1,10 +1,26 @@
 package org.top.rentalhousingcatalog;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
 
 // Контроллер - веб-интерфейс приложения
 @RestController
 public class ApplicationController {
     // репозиторий для работы с объектами (использование DI)
+    private final RentalHouseRepository rentalHouseRepository;
+    public ApplicationController(RentalHouseRepository rentalHouseRepository) {
+        this.rentalHouseRepository = rentalHouseRepository;
+    }
+    // дефолтные обработчики
+    @GetMapping("")
+    public String index() {
+        return LocalTime.now() + ": Сервер запущен и работает."
+    }
 
+    @GetMapping("ping")
+    public String ping() {
+        return "pong";
+    }
 }
