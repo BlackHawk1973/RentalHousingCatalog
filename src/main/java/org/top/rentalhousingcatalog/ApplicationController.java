f430af4957cd3589027a80b67d7b220423a5e045
Black Hawk <bhawk73@gmail.com>
2023-10-13 14:11:39 +0300

    Final commit

--- src/main/java/org/top/rentalhousingcatalog/ApplicationController.java
+++ src/main/java/org/top/rentalhousingcatalog/ApplicationController.java
@@ -1,9 +1,9 @@
 package org.top.rentalhousingcatalog;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalTime;
+import java.util.Optional;
 
 // Контроллер - веб-интерфейс приложения
 @RestController
@@ -16,11 +16,55 @@ public class ApplicationController {
     // дефолтные обработчики
     @GetMapping("")
     public String index() {
-        return LocalTime.now() + ": Сервер запущен и работает."
+        return LocalTime.now() + ": Сервер запущен и работает.";
     }
 
     @GetMapping("ping")
     public String ping() {
         return "pong";
+    }
+
+    // ОБРАБОТЧИКИ, ВЫЗЫВАЮЩИЕ ОПЕРАЦИИ ПО ВЗАИМОДЕЙСТВИЮ С БД
+    // 1. получить все записи
+    @GetMapping("houses")
+    public Iterable<RentalHouse> findAll() {
+        return rentalHouseRepository.findAll();
+    }
+
+    // 2. получить запись по id
+    @GetMapping("houses/{id}")
+    public Optional<RentalHouse> findById(@PathVariable Integer id) {
+        return rentalHouseRepository.findById(id);
+    }
+
+    // 3. добавить новую запись
+    @PostMapping("houses/new")
+    public RentalHouse addNew(@RequestBody RentalHouse rentalHouse) {
+        return rentalHouseRepository.save(rentalHouse);
+    }
+
+    // 4. удалить запись по id
+    @DeleteMapping("houses/{id}")
+    public Optional<RentalHouse> deleteById(@PathVariable Integer id) {
+        Optional<RentalHouse> deleted = rentalHouseRepository.findById(id); // пробуем найти удаляемый объект
+        if (deleted.isPresent()) {
+            // если он присутствует, то удалить его
+            rentalHouseRepository.deleteById(id);
+        }
+        return deleted; // вернуть удаляемый (вернется удаленный объект, либо null если объекта с таким id не было)
+    }
+
+    // 5. изменить запись
+    @PostMapping("houses/update")
+    public Optional<RentalHouse> updateExisting(@RequestBody RentalHouse rentalHouse) {
+        // 1. Проверяем есть ли такая запись
+        Optional<RentalHouse> updated = rentalHouseRepository.findById(rentalHouse.getId());
+        // 2. если онf есть, то обновляем ее данные
+        if (updated.isPresent()) {
+            rentalHouseRepository.save(rentalHouse);
+            return Optional.of(rentalHouse);
+        }
+        // 3. вернуть пустое значение
+        return Optional.empty();
     }
 }
